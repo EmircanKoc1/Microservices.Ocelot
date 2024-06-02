e11b6f68d74a14a2ecef21f0765c03bc4f8b65a6
Emircan <emircandv787@gmail.com>
2024-06-03 02:12:07 +0300

    added  claim based and normal authorization

--- API_Gateway/Program.cs
+++ API_Gateway/Program.cs
@@ -1,13 +1,34 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using Ocelot;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.AddJsonFile("ocelot.json");
 builder.Services.AddOcelot();
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!))
+        };
+    });
+
 var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 await app.UseOcelot();
 
--- Resources/API1/Program.cs
+++ Resources/API1/Program.cs
@@ -1,9 +1,52 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!))
+        };
+    });
+
+
+builder.Services.AddAuthorization(config =>
+{
+    config.AddPolicy("Authenticated", policy =>
+    {
+        policy.RequireAuthenticatedUser();
+
+    });
+    config.AddPolicy("AdminPolicy", policy =>
+    {
+        policy.RequireClaim("Role","Admin");
+    });
+});
+
 
 
 var app = builder.Build();
 
-app.MapGet("/", () => "api1");
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapGet("/", () => "api1")
+    .RequireAuthorization("Authenticated");
+
+app.MapGet("/admin", () => "admin")
+    .RequireAuthorization("AdminPolicy");
+
 
 
 
